2a0525694c4d30c4194c4eeaaa26d32e68c6782b
mohananagavenkat <mohananagavenkat@gmail.com>
2018-09-23 22:57:27 +0530

    serving application on port 5002

--- app.js
+++ app.js
@@ -9,12 +9,18 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // serving static assets
-app.use(express.static("/assets"));
+app.use(express.static("assets"));
 
 // Loading Routes
-
 const authRoutes = require("./routes/authRoutes");
+const generalRoutes = require("./routes/generalRoutes");
 
 // Using Routes
-
+app.use(generalRoutes);
 app.use("/auth", authRoutes);
+
+const port = 5002;
+
+app.listen(port,()=>{
+    console.log(`Application is serving on http://localhost:${port}`);
+})

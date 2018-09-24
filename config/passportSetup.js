56cc73051d22845cad342e717cc816d13091eb15
mohananagavenkat <mohananagavenkat@gmail.com>
2018-09-25 00:17:59 +0530

    Handling callback function which will be triggered after getting profile info, saving the user data in database if not exist

--- config/passportSetup.js
+++ config/passportSetup.js
@@ -4,6 +4,8 @@ const googleStrategy = require("passport-google-oauth20");
 
 const env = require("../.env");
 
+const User = require("../models/userModel");
+
 passport.use(
   new googleStrategy(env.google, function(
     accessToken,
@@ -12,5 +14,21 @@ passport.use(
     done
   ) {
     // this is passport callback function
+    User.findOne({ googleID: profile.id }).then(user => {
+      if (user) {
+        console.log("User Already exist");
+        console.table(user);
+      } else {
+        const newUser = new User({
+          name: profile.displayName,
+          googleID: profile.id
+        });
+        newUser.save().then(data => {
+          console.log("User Created Successfully");
+          console.table(data);
+        });
+      }
+    });
+    //console.log(profile);
   })
 );

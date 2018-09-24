0c40dd147e0a358de3f9b62cac1423b99858a70a
mohananagavenkat <mohananagavenkat@gmail.com>
2018-09-25 00:19:57 +0530

    using passport middleware to handle callback URI page after user has been redirected by google from google signin page

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -13,7 +13,7 @@ router.get("/google",passport.authenticate("google",{
     scope:['profile']
 }));
 
-router.get("/google/callback",(req,res)=>{
+router.get("/google/callback",passport.authenticate("google"),(req,res)=>{
     // This is url to which user will be redirected after google authentication
     res.send("This is callback page")
 });

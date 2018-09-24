b14d932412d0d617d306bbaca781a836f5bc048d
mohananagavenkat <mohananagavenkat@gmail.com>
2018-09-25 00:16:54 +0530

    created models folder and user model

---
+++ models/userModel.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    googleID:{
+        type:Number,
+        required:true
+    }
+});
+
+const userModel = mongoose.model("users",userSchema);
+
+module.exports = userModel;
